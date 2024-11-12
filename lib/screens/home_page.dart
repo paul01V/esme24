215c0446e2fa15667fc339c8695b3d7209d87a94
Jean Paul Jacquot <jeanpauljacquot@admins-MacBook-Pro.local>
2024-11-12 09:44:17 +0100

    wip

---
+++ lib/screens/home_page.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  final String title;
+
+  const HomePage({super.key, required this.title});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(child: Text(widget.title)),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        tooltip: 'Add plan',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
